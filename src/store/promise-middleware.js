c77efb02f18a7dfb567e186ef492f9c3b9d01752
Mark Myers <markalope@gmail.com>
2018-09-05 09:41:16 -0700

    OK gotta keep writing files. Starting on goals directory

--- src/store/promise-middleware.js
+++ src/store/promise-middleware.js
@@ -1,4 +1,4 @@
-import { LOADING_START, LOADING_END, ERROR } from '../components/app/reducers';
+import { LOADING_START, LOADING_END, ERROR } from '../components/app/app-reducers';
 
 const isPromise = val => val && typeof val.then === 'function';
 
