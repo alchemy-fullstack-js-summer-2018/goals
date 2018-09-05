afdd9827ec2da96669303162dea7fe25efd7e231
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-05 09:42:16 -0700

    Building out header a bit

--- src/components/app/Header.js
+++ src/components/app/Header.js
@@ -1,12 +1,30 @@
 import React, { PureComponent } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 class Header extends PureComponent {
 
+  static propTypes = {
+    user: PropTypes.object
+  }
+
   render() {
+    const { user } = this.props;
 
     return (
       <div>
-        <h3>Hello Header!</h3>
+        <h1>Life Goals</h1>
+        <nav>
+          <Link to="/">Home</Link>
+          &nbsp;
+          <Link to="/goals">Goals</Link>
+          &nbsp;
+          {
+            user
+              ? <Link to="/" onClick={this.handleLogout}>Logout</Link>
+              : <Link to="/auth">Login</Link>
+          }
+        </nav>
       </div>
     );
   }
