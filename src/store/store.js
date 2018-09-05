8230dba0c2b123dbdc631f347ce281030cfd22bf
rbtprograms <rbt61690@gmail.com>
2018-09-04 23:32:07 -0700

    implemented some more reducers

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,12 @@ import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import promiseMiddleware from './promise-middleware';
 import thunk from 'redux-thunk';
 import { user } from '../components/auth/reducers';
+import { loading, error } from '../components/app/reducers';
 
 const rootReducer = combineReducers({
-  user
+  user,
+  loading,
+  error
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
