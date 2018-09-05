47464801d9074922172a11efb1de5715edc9b52c
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-04 20:57:41 -0700

    Header and Home component scaffolding

---
+++ src/components/app/Header.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Header extends PureComponent {
+
+  render() {
+
+    return (
+      <h1>Hello Header!</h1>
+    );
+  }
+}
+
+export default Header;
---
+++ src/components/app/Home.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Home extends PureComponent {
+
+  render() {
+
+    return (
+      <h1>I am a home page</h1>
+    );
+  }
+}
+
+export default Home;
