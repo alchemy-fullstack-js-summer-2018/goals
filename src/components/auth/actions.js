1cddfa518ac90cd32e9992990bc1e72824fda232
rbtprograms <rbt61690@gmail.com>
2018-09-05 12:38:21 -0700

    pushing to submit

--- src/components/auth/actions.js
+++ src/components/auth/actions.js
@@ -1,5 +1,6 @@
-import { USER_AUTH } from './reducers';
-// import { verifyUser } from '../../services/api';
+import { USER_AUTH, LOGOUT, CHECKED_AUTH } from './reducers';
+import { verifyUser } from '../../services/api';
+import { getStoredUser, clearStoredUser } from '../../services/request';
 
 import {
   signup as signupApi,
@@ -13,3 +14,26 @@ const makeAuth = api => credentials => ({
 
 export const signup = makeAuth(signupApi);
 export const signin = makeAuth(signinApi);
+
+export const logout = () => ({ type: LOGOUT });
+
+const authChecked = () => ({ type: CHECKED_AUTH });
+
+export const tryLoadUser = () => dispatch => {
+  const user = getStoredUser();
+  if(!user || !user.token) {
+    return dispatch(authChecked());
+  }
+
+  verifyUser(user.token)
+    .then(() => dispatch({
+      type: USER_AUTH,
+      payload: user
+    }))
+    .catch(() => {
+      clearStoredUser();
+    })
+    .then(() => {
+      dispatch(authChecked());
+    });
+};
---
+++ src/components/goals/GoalItem.js
@@ -0,0 +1,29 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getGoalById } from './reducers';
+import { Link } from 'react-router-dom';
+
+class GoalItem extends PureComponent {
+
+    static propTypes = {
+      goal: PropTypes.object
+    };
+
+    render() { 
+      const { goal } = this.props;
+
+      return (
+        <li>
+          <Link to={`/goal/${goal._id}`}>{goal.title}</Link>
+        </li>
+      );
+    }
+}
+ 
+export default connect(
+  (state, { id }) => ({
+    goal: getGoalById(state, id)
+  }),
+  null
+)(GoalItem);
