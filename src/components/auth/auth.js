196199c98ff58150cb7a4f66b58ff1b18dada37f
MariahAdams <mariah.adams23@yahoo.com>
2018-09-06 09:41:36 -0700

    adding signin/signup paths to auth component

--- src/components/auth/auth.js
+++ src/components/auth/auth.js
@@ -1,14 +1,38 @@
-import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
+import React, { PureComponent } from 'react';
+import { Switch, Route, Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import Credentials from './Credentials';
+// import styles from './Auth.css';
 
-class Auth extends Component {
+class Auth extends PureComponent {
+
+  static propTypes = {
+    signin: PropTypes.func.isRequired,
+    signup: PropTypes.func.isRequired
+  };
     
   render() { 
+    const { signin, signup } = this.props;
 
     return (
-      <div>
+      <section>
         <h2>Auth Component</h2>
-      </div>
+        <Switch>
+          <Route path="/auth/signin" component={() => (
+            <div>
+              <p>Not yet registered? <Link to="/auth/signup">Sign Up</Link></p>
+              <Credentials action="Sign In" submit={signin}/>
+            </div>
+          )}/>
+          <Route path="/auth/signup" component={() => (
+            <div>
+              <p>Already have an account? <Link to="/auth/signin">Sign In</Link></p>
+              <Credentials action="Sign Up" submit={signup}/>
+            </div>
+          )}/>
+          <Redirect to="/auth/signin"/>
+        </Switch>
+      </section>
     );
   }
 }
