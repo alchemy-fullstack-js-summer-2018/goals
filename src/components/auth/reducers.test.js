b426559a0c9a57e9cf5bcd43b039d0f78e7c9ecb
carriecarter <carrie@carrie-carter.com>
2018-09-06 11:43:01 -0700

    reducers testing in progress

---
+++ src/components/auth/reducers.test.js
@@ -0,0 +1,18 @@
+import { 
+  user, getUser,
+  USER_AUTH, LOGOUT } from './reducers';
+
+describe('user reducer', () => {
+
+  it('initializers to null', () => {
+    const state = user(undefined, {});
+    expect(state).toBe(null);
+  });
+
+  it('loads user', () => {
+    const data = { name: 'user' };
+    const state = user(null, { type: USER_AUTH, payload: data });
+    expect(state).toEqual(data);
+  });
+
+});
