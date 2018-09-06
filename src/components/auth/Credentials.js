043f14954a960892eb69a00f740be8ad58c5062f
MariahAdams <mariah.adams23@yahoo.com>
2018-09-06 09:07:03 -0700

    added credentials

---
+++ src/components/auth/Credentials.js
@@ -0,0 +1,59 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+// import styles from './Credentials.css';
+import FormControl from '../shared/FormControl';
+// TODO: allowName, Form CSS
+
+class Credentials extends PureComponent {
+
+  static propTypes = {
+    submit: PropTypes.func.isRequired,
+    action: PropTypes.string.isRequired,
+    // allowName: PropTypes.bool
+  };
+
+  state = {
+    name: '',
+    email: '',
+    password: ''
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.submit(this.state)
+      .catch(() => {});
+  };
+  
+  render() { 
+    const { action } = this.props;
+    const { name, email, password } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+
+        <FormControl label="name">
+          <input name="name" value={name} onChange={this.handleChange}></input>
+        </FormControl>
+        
+        <FormControl label="email">
+          <input name="email" value={email} onChange={this.handleChange}></input>
+        </FormControl>
+
+        <FormControl label="password">
+          <input name="password" typpe="password" value={password} onChange={this.handleChange}></input>
+        </FormControl>
+
+        <FormControl>
+          <button>{action}</button>
+        </FormControl>
+
+      </form>
+    );
+  }
+}
+ 
+export default Credentials;
