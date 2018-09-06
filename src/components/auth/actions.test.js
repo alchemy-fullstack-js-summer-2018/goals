20d179d1c78748250e085be77bd5f6aa7a20b725
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-06 12:00:40 -0700

    Make first auth actions test and verify it fails

--- src/components/auth/actions.test.js
+++ src/components/auth/actions.test.js
@@ -3,3 +3,26 @@ jest.mock('../../services/goalsApi', () => ({
   signup: jest.fn(),
   verify: jest.fn()
 }));
+
+import { signup, signin } from './actions';
+import { USER_AUTH } from './reducers';
+import { signup as signupSvc, signin as signinSvc } from '../../services/goalsApi';
+
+describe('Auth action creators', () => {
+  function testAuth(name, mockSvc, actionCreator) {
+    it(`creates ${name} action`, () => {
+      const promise = Promise.resolve();
+      mockSvc.mockReturnValueOnce(promise);
+
+      const credentials = {};
+      const { type, payload } = actionCreator(credentials);
+      expect(type).toBe(USER_AUTH);
+      expect(payload).toBe(promise);
+      expect(mockSvc.mock.calls.length).toBe(1);
+      expect(mockSvc.mock.calls[0][0]).toBe(credentials);
+    });
+  }
+
+  testAuth('signup', signupSvc, signup);
+  testAuth('signin', signinSvc, signin);
+});
