6c3d8b1bd1e93d5f00fcdd08baa8e715c4a55cb4
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-06 15:47:53 -0700

    GOALS_ADD reducer test successful

--- src/components/goals/reducers.js
+++ src/components/goals/reducers.js
@@ -1,11 +1,17 @@
 
 
 export const GOALS_LOAD = 'GOALS_LOAD';
+export const GOALS_ADD = 'GOALS_ADD';
 
-export function goalsById(state = [], { type, payload }) {
+export function goals(state = [], { type, payload }) {
   switch(type) {
     case GOALS_LOAD:
       return payload;
+    case GOALS_ADD:
+      return [
+        ...state,
+        payload
+      ];
     default:
       return state;
   }
--- src/components/goals/reducers.test.js
+++ src/components/goals/reducers.test.js
@@ -1,24 +1,39 @@
 import {
-  goalsById,
-  GOALS_LOAD
+  goals,
+  GOALS_LOAD,
+  GOALS_ADD,
 } from './reducers';
 
 describe('Goals reducers', () => {
   
   it('Initialize to empty array', () => {
-    const state = goalsById(undefined, {});
+    const state = goals(undefined, {});
     expect(state).toEqual([]);
   });
 
   it('Loads goals', () => {
     const payload = [{}, {}, {}];
 
-    const state = goalsById([], {
+    const state = goals([], {
       type: GOALS_LOAD,
       payload
     });
 
     expect(state).toBe(payload);
+  });
+
+  it('Adds a goal', () => {
+    const goal1 = { name: 'Profit', description: '$$$', completed: false };
+    const goal2 = { name: 'Learn', description: 'Get edumacated', completed: true };
+    const goal3 = { name: 'Mentor', description: 'Edumucate peoples', completed: false };
+
+    const state = goals([goal1, goal2], {
+      type: GOALS_ADD,
+      payload: goal3
+    });
+    
+    expect(state).toEqual([goal1, goal2, goal3]);
+
   });
 
 
