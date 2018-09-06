f6e4f00a92ddf143bae9af9f8179c7d54967d3ae
MariahAdams <mariah.adams23@yahoo.com>
2018-09-05 17:40:03 -0700

    setting up auth component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import Header from './Header';
 import Home from './Home';
+import Auth from '../auth/auth';
 import styles from './App.css';
 
 class App extends Component {
@@ -13,13 +14,14 @@ class App extends Component {
         <Fragment>
           <div className={styles.app}>
             <header>
-              <h1>Hello React</h1>
+              <h1>Gotta Have Goals</h1>
               <Header/>
             </header>
 
             <main>
               <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/auth" component={Auth}/>
                 <Redirect to="/"/>
               </Switch>
             </main>
---
+++ src/components/auth/auth.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Auth extends Component {
+    
+    render() { 
+
+        return (
+            <div>
+                <h2>Auth Component</h2>
+            </div>
+        );
+    }
+}
+ 
+export default Auth;
