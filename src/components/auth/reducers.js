b17485dd565eca27108df3ce9c4947cae32e4878
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-06 11:38:04 -0700

    Write case for user auth reducer and verify test will pass

--- src/components/auth/reducers.js
+++ src/components/auth/reducers.js
@@ -5,8 +5,10 @@ export const CHECKED_AUTH = 'CHECKED_AUTH';
 export const getUser = state => state.user;
 export const getCheckedAuth = state => state.checkedAuth;
 
-export function user(state = null, { type }) {
+export function user(state = null, { type, payload }) {
   switch(type) {
+    case USER_AUTH:
+      return payload;
     default:
       return state;
   }
