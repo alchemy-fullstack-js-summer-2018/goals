33d0a5f400a2451ac31b1b2650dd5464b0eae161
MariahAdams <mariah.adams23@yahoo.com>
2018-09-06 09:57:09 -0700

    updated scripts

--- src/components/auth/reducers.test.js
+++ src/components/auth/reducers.test.js
@@ -1,8 +1,9 @@
-import {} from './reducers';
+import { user } from './reducers';
 
 describe('user reducers', () => {
 
   it('initializes to null', () => {
-    
+    const state = user(undefined, {});
+    expect(state).toBe(null);
   });
 });
