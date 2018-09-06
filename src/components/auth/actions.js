7d3497e9f6bae14cd485c196e609c60341c9ca72
Kevin Grayson <k.grayson.ms@gmail.com>
2018-09-06 13:47:11 -0700

    Updating auth actions

--- src/components/auth/actions.js
+++ src/components/auth/actions.js
@@ -1,10 +1,10 @@
 import { USER_AUTH, LOGOUT, CHECKED_AUTH } from './reducers';
-// import { verifyUser } from '../../services/api';
-// import { getStoredUser, clearStoredUser } from '../../services/request';
+import { verifyUser } from '../../services/api';
+import { getStoredUser, clearStoredUser } from '../../services/request';
 
 import {
   signup as signupApi,
-  signin as signinApi
+  signin as signinApi,
 } from '../../services/api';
 
 // // Use of higher-order-functions (HOF) to have two api calls with same
@@ -28,3 +28,24 @@ export const signin = credentials => ({
 });
 
 export const logout = () => ({ type: LOGOUT });
+
+const authChecked = () => ({ type: CHECKED_AUTH });
+
+export const tryLoadUser = () => dispatch => {
+  const user = getStoredUser();
+  if(!user || !user.token) {
+    return dispatch(authChecked());
+  }
+
+  verifyUser(user.token)
+    .then(() => dispatch({
+      type: USER_AUTH,
+      payload: user
+    }))
+    .catch(() => {
+      clearStoredUser();
+    })
+    .then(() => {
+      dispatch(authChecked);
+    });
+};
--- src/components/auth/reducers.js
+++ src/components/auth/reducers.js
@@ -3,6 +3,7 @@ export const LOGOUT = 'LOGOUT';
 export const CHECKED_AUTH = 'CHECKED_AUTH';
 
 export const getUser = state => state.user;
+export const getCheckedAuth = state => state.checkedAuth;
 
 export function user(state = null, { type, payload }) {
   switch(type) {
@@ -10,6 +11,15 @@ export function user(state = null, { type, payload }) {
       return payload;
     case LOGOUT:
       return null;
+    default:
+      return state;
+  }
+}
+
+export function checkedAuth(state = false, { type }) {
+  switch(type) {
+    case CHECKED_AUTH:
+      return true;
     default:
       return state;
   }
