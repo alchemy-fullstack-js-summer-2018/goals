cd75416a0cb2cfbffdad4192b444937740cadfdb
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-06 12:49:49 -0700

    Add auth reducers to store

--- src/store/store.js
+++ src/store/store.js
@@ -2,10 +2,13 @@ import { applyMiddleware, createStore, combineReducers, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './promiseMiddleware';
 import { loading, error } from '../components/app/reducers';
+import { user, checkedAuth } from '../components/auth/reducers';
 
 const reducer = combineReducers({
   loading,
-  error
+  error,
+  user,
+  checkedAuth
 });
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
