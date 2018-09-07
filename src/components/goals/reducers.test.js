e33b3b3398b62ef5fa55c86f3a164c2298557998
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-07 13:01:00 -0700

    Add PrivateRoute component and remove unused test

--- src/components/goals/reducers.test.js
+++
@@ -1,8 +0,0 @@
-import { goals } from './reducers';
-
-describe('Goals reducers', () => {
-  it('loads initial state', () => {
-    const state = goals(undefined, {});
-    expect(state).toEqual([]);
-  });
-});
