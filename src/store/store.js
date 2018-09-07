33ddce9e8b843642862f165ae62012d5b4b5fb64
Rehmsy <sarahrehmer@gmail.com>
2018-09-06 20:11:33 -0700

    closing vscode for real.

--- src/store/store.js
+++ src/store/store.js
@@ -2,15 +2,15 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import promiseMiddleware from './promise-middleware';
-//import { error, loading } from '../components/app/reducers';
+import { error, loading } from '../components/app/reducers';
 //import { user, checkedAuth } from '../components/auth/reducers';
 import { goalList, goalsById } from '../components/goals/reducers';
 
 const rootReducer = combineReducers({
-  // error,
-  // loading,
-  // user,
-  // checkedAuth,
+  error,
+  loading,
+  //user,
+  //checkedAuth,
   goalList,
   goalsById
 });
