6c6adb9a05d6f838c1b4c2c2fb5faefaa6a07c7c
injoongyoon <27440940+AttilaTheHen@users.noreply.github.com>
2018-09-07 12:15:14 -0700

    Add actions and reducers for goals and update goalsApi

--- src/services/goalsApi.js
+++ src/services/goalsApi.js
@@ -1,4 +1,4 @@
-import { get, post } from './request';
+import { get, post, put } from './request';
 
 const URL = '/api';
 const GOALS_URL = `${URL}/me/goals`;
@@ -7,6 +7,9 @@ const AUTH_URL = `${URL}/auth`;
 export const getAllGoals = () => get(GOALS_URL);
 export const signin = credentials => post(`${AUTH_URL}/signin`, credentials);
 export const signup = credentials => post(`${AUTH_URL}/signup`, credentials);
+
+export const postGoal = data => post(GOALS_URL, data);
+export const updateGoal = data => put(GOALS_URL, data);
 
 export const verifyUser = token => get(`${AUTH_URL}/verify`, {
   headers: {
